d807f39d2b5bdea47f4d00f309497222fb2213ee
Markus Felten <Markus.Felten@gmx.de>
2016-09-26 20:47:41 +0200

    chore(npm): preps for npm release

--- api.js
+++ api.js
@@ -1,42 +1,48 @@
+/* jslint node: true, esnext: true */
+
 'use strict';
 
 const net = require('net'),
-      msgpack = require('msgpack');
+  msgpack = require('msgpack');
 
-const HOST = '::1', PORT = 1337,
-      socket = new net.Socket(),
-      msgpackStream = new msgpack.Stream(socket),
-      promiseQueue = [];
+const HOST = '::1',
+  PORT = 1337,
+  socket = new net.Socket(),
+  msgpackStream = new msgpack.Stream(socket),
+  promiseQueue = [];
 
-msgpackStream.addListener('msg', function(data) {
-    promiseQueue.shift().resolve(data);
+msgpackStream.addListener('msg', function (data) {
+  promiseQueue.shift().resolve(data);
 });
 
-socket.on('error', function(error) {
-    console.log('Error');
-    socket.destroy();
+socket.on('error', function (error) {
+  console.log('Error');
+  socket.destroy();
 });
 
-socket.on('close', function() {
-    console.log('Lost connection');
-    for(var i = 0; i < promiseQueue.length; ++i)
-        promiseQueue[i].reject();
-    promiseQueue.length = 0;
+socket.on('close', function () {
+  console.log('Lost connection');
+  for (var i = 0; i < promiseQueue.length; ++i)
+    promiseQueue[i].reject();
+  promiseQueue.length = 0;
 });
 
-socket.connect(PORT, HOST, function() {
-    request('query', 9, 1, 2, 0).then(function(data) {
-        console.log('Then', data);
-    }, function() {
-        console.log('Then failed');
+socket.connect(PORT, HOST, function () {
+  request('query', 9, 1, 2, 0).then(function (data) {
+    console.log('Then', data);
+  }, function () {
+    console.log('Then failed');
+  });
+});
+
+const request = function () {
+  const packet = msgpack.pack(Array.from(arguments));
+  return new Promise(function (resolve, reject) {
+    promiseQueue.push({
+      'resolve': resolve,
+      'reject': reject
     });
-});
-
-const request = function() {
-    const packet = msgpack.pack(Array.from(arguments));
-    return new Promise(function(resolve, reject) {
-        promiseQueue.push({'resolve':resolve, 'reject':reject});
-        socket.write(packet);
-        console.log(packet);
-    });
+    socket.write(packet);
+    console.log(packet);
+  });
 };
