2f587ec412ce2edb7b588d4924bd5b3e89661639
Markus Felten <Markus.Felten@gmx.de>
2016-10-02 21:10:41 +0200

    test: run SymatemAPI from within mocha

--- tests/simple_test.js
+++ tests/simple_test.js
@@ -1,4 +1,4 @@
-/* global describe, it, xit */
+/* global describe, it, xit, before, after */
 /* jslint node: true, esnext: true */
 
 'use strict';
@@ -8,9 +8,31 @@ const chai = require('chai'),
   expect = chai.expect,
   should = chai.should();
 
-const api = require('../api');
+const path = require('path'),
+  spawn = require('child_process').spawn,
+  api = require('../api');
 
 describe('connection', () => {
+  let symatem;
+  before(() => {
+    symatem = spawn(path.join(__dirname, '..', 'SymatemAPI'), ['aFile']);
+
+    symatem.stdout.on('data', (data) => console.log(`stdout: ${data}`));
+    symatem.stderr.on('data', (data) => console.log(`stderr: ${data}`));
+    symatem.on('close', (code) => {
+      symatem = undefined;
+      console.log(`child process exited with code ${code}`);
+    });
+    symatem.on('error', (err) => console.log('Failed to start child process.'));
+  });
+
+  after(() => {
+    if (symatem) {
+      symatem.kill();
+      symatem = undefined;
+    }
+  });
+
   const cp = api.open();
 
   it('opens', () =>
@@ -19,7 +41,7 @@ describe('connection', () => {
     )
   );
 
-  it('query', () =>
+  xit('query', () =>
     cp.then(connection =>
       connection.query(9, 1, 2, 0).then(data => assert.deepEqual(data, [589]))
     )
