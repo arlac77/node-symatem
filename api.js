8a0810aae2f3fb5d3c0f665b6de624613b6a5ac1
Markus Felten <Markus.Felten@gmx.de>
2016-10-03 14:54:29 +0200

    feat: define and use queryMask

--- api.js
+++ api.js
@@ -5,6 +5,14 @@
 const net = require('net'),
   msgpack = require('msgpack');
 
+exports.queryMode = ['M', 'V', 'I'];
+exports.queryMask = {};
+
+for (let i = 0; i < 27; ++i) {
+  const key = exports.queryMode[i % 3] + exports.queryMode[(i / 3) % 3] + exports.queryMode[(i / 9) % 3];
+  exports.queryMask[key] = i;
+}
+
 exports.open = function (host = '::1', port = 1337) {
   const socket = new net.Socket(),
     msgpackStream = new msgpack.Stream(socket),
@@ -21,12 +29,29 @@ exports.open = function (host = '::1', port = 1337) {
         socket.write(packet);
         console.log('sending', packet);
       });
-    }
+    },
+    upload: (text) =>
+      Promise.all([connection.createSymbol(), connection.createSymbol()]).then(args => {
+        const [textSymbol, packageSymbol] = args;
+        return connection.setBlobSize(textSymbol, text.length * 8).then(() =>
+          connection.writeBlob(textSymbol, 0, text.length * 8, Buffer.from(text)).then(() =>
+            connection.link(textSymbol, 28, 32).then(() =>
+              connection.deserializeBlob(textSymbol, packageSymbol).then(data => {
+                console.log(data);
+                connection.releaseSymbol(textSymbol);
+                return packageSymbol;
+              })
+            )
+          )
+        );
+      })
+
   };
 
   [
     'createSymbol', 'releaseSymbol',
-    'getBlobSize', 'setBlobSize', 'decreaseBlobSize', 'increaseBlobSize', 'readBlob', 'writeBlob', 'deserializeBlob',
+    'getBlobSize', 'setBlobSize', 'decreaseBlobSize', 'increaseBlobSize', 'readBlob', 'writeBlob',
+    'deserializeBlob',
     'query', 'link', 'unlink'
   ].forEach(
     name => {
--- tests/simple_test.js
+++ tests/simple_test.js
@@ -15,22 +15,19 @@ const path = require('path'),
 let symatem;
 
 
+
 beforeEach('start SymatemAPI', done => {
   console.log('start SymatemAPI');
   symatem = spawn(path.join(__dirname, '..', 'SymatemAPI'), [path.join(__dirname, 'test.sdb')]);
 
   symatem.stdout.on('data', data => console.log(`stdout: ${data}`));
   symatem.stderr.on('data', data => console.log(`stderr: ${data}`));
-  symatem.on('close', code => {
-    symatem = undefined;
-    console.log(`child process exited with code ${code}`);
-  });
   symatem.on('error', err => console.log(`Failed to start child process. ${err}`));
 
   setTimeout(() => done(), 500);
 });
 
-afterEach(done => {
+afterEach('stop SymatemAPI', done => {
   if (symatem) {
     symatem.on('close', code => {
       symatem = undefined;
@@ -42,16 +39,25 @@ afterEach(done => {
   }
 });
 
+
 describe('connection', () => {
   it('opens', () =>
     api.open().then(connection =>
-      connection.query(false, 9, 1, 2, 0).then(data => assert.deepEqual(data, [589]))
+      connection.query(false, api.queryMask.MMV, 1, 2, 0).then(data => assert.deepEqual(data, [589]))
     )
   );
 
   xit('query', () =>
     api.open().then(connection =>
-      connection.query(false, 9, 1, 2, 0).then(data => assert.deepEqual(data, [589]))
+      connection.query(false, api.queryMask.MMV, 1, 2, 0).then(data => assert.deepEqual(data, [589]))
+    )
+  );
+
+  xit('upload', () =>
+    api.open().then(connection =>
+      connection.upload('(Entity; Attribute Value;)')
+      .then(symbol => connection.query(true, api.queryMask.MVI, symbol, 0, 0))
+      .then(result => assert.deepEqual(result, [1]))
     )
   );
 });
