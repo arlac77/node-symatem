c8a189ecf6790067c8e0c7ebfe556ad2380dd579
Markus Felten <Markus.Felten@gmx.de>
2016-10-04 01:09:07 +0200

    test: cleanup

--- tests/simple_test.js
+++ tests/simple_test.js
@@ -9,47 +9,23 @@ const chai = require('chai'),
   should = chai.should();
 
 const path = require('path'),
+  fs = require('fs'),
   spawn = require('child_process').spawn,
   api = require('../api');
 
 let symatem;
 
 
-beforeEach('start SymatemAPI', done => {
-  symatem = spawn(path.join(__dirname, '..', 'SymatemAPI'), [path.join(__dirname, 'test.sdb')]);
 
-  symatem.stdout.on('data', data => console.log(`stdout: ${data}`));
-  symatem.stderr.on('data', data => console.log(`stderr: ${data}`));
-  symatem.on('error', err => console.log(`Failed to start child process. ${err}`));
-
-  setTimeout(() => done(), 300);
-});
-
-afterEach('stop SymatemAPI', done => {
-  if (symatem) {
-    symatem.on('close', code => {
-      symatem = undefined;
-      console.log(`child process exited with code ${code}`);
-      //done();
-    });
-
-    symatem.kill();
-    done();
-
-  } else {
-    done();
-  }
+describe('connection problems', () => {
+  it('should fail', () =>
+    api.open('127.0.0.1', 12345)
+    .catch(e => assert.equal(e.code, 'ECONNREFUSED'))
+  );
 });
 
 describe('connection', () => {
-  describe('wrong connection', () => {
-    xit('should fail', () =>
-      api.open('127.0.0.1', 12345)
-      .catch(e => assert.equal(e.code, 'ECONNREFUSED'))
-    );
-  });
-
-  it('opens', () =>
+  xit('opens', () =>
     api.open().then(connection =>
       connection.query(false, api.queryMask.MMV, 1, 2, 0).then(data => assert.deepEqual(data, [589]))
     )
@@ -61,7 +37,7 @@ describe('connection', () => {
     )
   );
 
-  xit('upload', () =>
+  it('upload', () =>
     api.open().then(connection =>
       connection.upload('(Entity; Attribute Value;)')
       //.then(() => connection.symbolNamed('Entity'))
@@ -71,4 +47,33 @@ describe('connection', () => {
       .then(result => assert.deepEqual(result, [13, 28]))
     )
   );
+
+  beforeEach('start SymatemAPI', done => {
+    const store = path.join(__dirname, 'test.sdb');
+    fs.unlink(store, (error) => {
+      symatem = spawn(path.join(__dirname, '..', 'SymatemAPI'), [store]);
+
+      symatem.stdout.on('data', data => console.log(`stdout: ${data}`));
+      symatem.stderr.on('data', data => console.log(`stderr: ${data}`));
+      symatem.on('error', err => console.log(`Failed to start child process. ${err}`));
+
+      setTimeout(() => done(), 300);
+    });
+  });
+
+  afterEach('stop SymatemAPI', done => {
+    if (symatem) {
+      symatem.on('close', code => {
+        symatem = undefined;
+        console.log(`child process exited with code ${code}`);
+        //done();
+      });
+
+      symatem.kill();
+      done();
+
+    } else {
+      done();
+    }
+  });
 });
