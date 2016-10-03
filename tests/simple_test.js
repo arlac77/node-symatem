e03a9ba508600034f1b4b883489d58947f17fca2
Markus Felten <Markus.Felten@gmx.de>
2016-10-03 11:45:33 +0200

    test: use 5 params query()

--- tests/simple_test.js
+++ tests/simple_test.js
@@ -1,4 +1,4 @@
-/* global describe, it, xit, before, after */
+/* global describe, it, xit, before, beforeEach, after, afterEach */
 /* jslint node: true, esnext: true */
 
 'use strict';
@@ -12,39 +12,46 @@ const path = require('path'),
   spawn = require('child_process').spawn,
   api = require('../api');
 
-describe('connection', function() {
-  let symatem;
-  before(function() {
-    console.log('start SymatemAPI');
-    symatem = spawn(path.join(__dirname, '..', 'SymatemAPI'), ['aFile']);
+let symatem;
 
-    symatem.stdout.on('data', (data) => console.log(`stdout: ${data}`));
-    symatem.stderr.on('data', (data) => console.log(`stderr: ${data}`));
-    symatem.on('close', (code) => {
+
+beforeEach('start SymatemAPI', done => {
+  console.log('start SymatemAPI');
+  symatem = spawn(path.join(__dirname, '..', 'SymatemAPI'), [path.join(__dirname, 'test.sdb')]);
+
+  symatem.stdout.on('data', data => console.log(`stdout: ${data}`));
+  symatem.stderr.on('data', data => console.log(`stderr: ${data}`));
+  symatem.on('close', code => {
+    symatem = undefined;
+    console.log(`child process exited with code ${code}`);
+  });
+  symatem.on('error', err => console.log(`Failed to start child process. ${err}`));
+
+  setTimeout(() => done(), 500);
+});
+
+afterEach(done => {
+  if (symatem) {
+    symatem.on('close', code => {
       symatem = undefined;
       console.log(`child process exited with code ${code}`);
+      done();
     });
-    symatem.on('error', (err) => console.log('Failed to start child process.'));
-  });
 
-  after(function() {
-    if (symatem) {
-      symatem.kill();
-      symatem = undefined;
-    }
-  });
+    symatem.kill();
+  }
+});
 
-  const cp = api.open();
-
+describe('connection', () => {
   it('opens', () =>
-    cp.then(connection =>
-      connection.query(9, 1, 2, 0).then(data => assert.deepEqual(data, [589]))
+    api.open().then(connection =>
+      connection.query(false, 9, 1, 2, 0).then(data => assert.deepEqual(data, [589]))
     )
   );
 
   xit('query', () =>
-    cp.then(connection =>
-      connection.query(9, 1, 2, 0).then(data => assert.deepEqual(data, [589]))
+    api.open().then(connection =>
+      connection.query(false, 9, 1, 2, 0).then(data => assert.deepEqual(data, [589]))
     )
   );
 });
