7970abbc33067949fe9dcdb319edabc5052b9b6b
Markus Felten <Markus.Felten@gmx.de>
2016-10-03 18:02:00 +0200

    feat(open): reject in case of error

--- api.js
+++ api.js
@@ -14,63 +14,63 @@ for (let i = 0; i < 27; ++i) {
 }
 
 exports.open = function (host = '::1', port = 1337) {
-  const socket = new net.Socket(),
-    msgpackStream = new msgpack.Stream(socket),
-    promiseQueue = [];
+  return new Promise((fullfill, reject) => {
+    const socket = new net.Socket(),
+      msgpackStream = new msgpack.Stream(socket),
+      promiseQueue = [];
 
-  const connection = {
-    request: (...args) => {
-      const packet = msgpack.pack(args);
-      return new Promise((resolve, reject) => {
-        promiseQueue.push({
-          resolve, reject
+    const connection = {
+      request: (...args) => {
+        const packet = msgpack.pack(args);
+        return new Promise((resolve, reject) => {
+          promiseQueue.push({
+            resolve, reject
+          });
+          socket.write(packet);
+          console.log('sending', packet);
         });
-        socket.write(packet);
-        console.log('sending', packet);
-      });
-    },
-    upload: (text) =>
-      Promise.all([connection.createSymbol(), connection.createSymbol()]).then(args => {
-        const [textSymbol, packageSymbol] = args;
-        return connection.setBlobSize(textSymbol, text.length * 8).then(() =>
-          connection.writeBlob(textSymbol, 0, text.length * 8, Buffer.from(text)).then(() =>
-            connection.link(textSymbol, 28, 32).then(() =>
-              connection.deserializeBlob(textSymbol, packageSymbol).then(data => {
-                console.log(data);
-                connection.releaseSymbol(textSymbol);
-                return packageSymbol;
-              })
+      },
+      upload: (text) =>
+        Promise.all([connection.createSymbol(), connection.createSymbol()]).then(args => {
+          const [textSymbol, packageSymbol] = args;
+          return connection.setBlobSize(textSymbol, text.length * 8).then(() =>
+            connection.writeBlob(textSymbol, 0, text.length * 8, Buffer.from(text)).then(() =>
+              connection.link(textSymbol, 28, 32).then(() =>
+                connection.deserializeBlob(textSymbol, packageSymbol).then(data => {
+                  console.log(data);
+                  connection.releaseSymbol(textSymbol);
+                  return packageSymbol;
+                })
+              )
             )
-          )
-        );
-      })
-  };
+          );
+        })
+    };
 
-  [
-    'createSymbol', 'releaseSymbol',
-    'getBlobSize', 'setBlobSize', 'decreaseBlobSize', 'increaseBlobSize', 'readBlob', 'writeBlob',
-    'deserializeBlob',
-    'query', 'link', 'unlink'
-  ].forEach(
-    name => {
-      connection[name] = (...args) => connection.request(name, ...args);
+    [
+      'createSymbol', 'releaseSymbol',
+      'getBlobSize', 'setBlobSize', 'decreaseBlobSize', 'increaseBlobSize', 'readBlob', 'writeBlob',
+      'deserializeBlob',
+      'query', 'link', 'unlink'
+    ].forEach(
+      name => {
+        connection[name] = (...args) => connection.request(name, ...args);
+      });
+
+    socket.on('data', data => console.log('received', data));
+    msgpackStream.addListener('msg', data => promiseQueue.shift().resolve(data));
+
+    socket.on('error', error => {
+      socket.destroy();
+      reject(error);
     });
 
-  socket.on('data', data => console.log('received', data));
-  msgpackStream.addListener('msg', data => promiseQueue.shift().resolve(data));
+    socket.on('close', () => {
+      promiseQueue.forEach(p => p.reject());
+      promiseQueue.length = 0;
+      socket.destroy();
+    });
 
-  socket.on('error', error => {
-    console.log(`error: ${error}`);
-    socket.destroy();
-  });
-
-  socket.on('close', () => {
-    promiseQueue.forEach(p => p.reject());
-    promiseQueue.length = 0;
-    socket.destroy();
-  });
-
-  return new Promise((fullfill, reject) => {
     socket.connect(port, host, error => {
       if (error) {
         reject(error);
--- tests/simple_test.js
+++ tests/simple_test.js
@@ -14,17 +14,14 @@ const path = require('path'),
 
 let symatem;
 
-
-
 beforeEach('start SymatemAPI', done => {
-  console.log('start SymatemAPI');
   symatem = spawn(path.join(__dirname, '..', 'SymatemAPI'), [path.join(__dirname, 'test.sdb')]);
 
   symatem.stdout.on('data', data => console.log(`stdout: ${data}`));
   symatem.stderr.on('data', data => console.log(`stderr: ${data}`));
   symatem.on('error', err => console.log(`Failed to start child process. ${err}`));
 
-  setTimeout(() => done(), 500);
+  setTimeout(() => done(), 300);
 });
 
 afterEach('stop SymatemAPI', done => {
@@ -32,15 +29,26 @@ afterEach('stop SymatemAPI', done => {
     symatem.on('close', code => {
       symatem = undefined;
       console.log(`child process exited with code ${code}`);
-      done();
+      //done();
     });
 
     symatem.kill();
+    done();
+
+  } else {
+    done();
   }
 });
 
 
 describe('connection', () => {
+  describe('wrong connection', () => {
+    it('should fail', () =>
+      api.open('127.0.0.1', 12345)
+      .catch(e => assert.equal(e.code, 'ECONNREFUSED'))
+    );
+  });
+
   it('opens', () =>
     api.open().then(connection =>
       connection.query(false, api.queryMask.MMV, 1, 2, 0).then(data => assert.deepEqual(data, [589]))
